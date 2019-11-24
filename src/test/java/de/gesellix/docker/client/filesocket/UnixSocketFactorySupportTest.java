b02218ed8de84a5cd7ecf6ad202d38f104a40702
Tobias Gesellchen <tobias@gesellix.de>
2019-11-24 21:47:37 +0100

    Check UnixSocketSupport os specific variants only on the relevant os'

--- src/test/java/de/gesellix/docker/client/filesocket/UnixSocketFactorySupportTest.java
+++ src/test/java/de/gesellix/docker/client/filesocket/UnixSocketFactorySupportTest.java
@@ -2,6 +2,8 @@ package de.gesellix.docker.client.filesocket;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,18 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class UnixSocketFactorySupportTest {
 
     @Test
+    @EnabledOnOs(OS.MAC)
     @DisplayName("Supports Mac OS X")
     void supportsMacOsX() {
         assertTrue(new UnixSocketFactorySupport().isSupported("Mac OS X"));
     }
 
     @Test
+    @EnabledOnOs(OS.LINUX)
     @DisplayName("Supports Linux")
     void supportsLinux() {
         assertTrue(new UnixSocketFactorySupport().isSupported("Linux"));
     }
 
     @Test
+    @EnabledOnOs(OS.WINDOWS)
     @DisplayName("Doesn't support Windows 10")
     void doesNotSupportWindows10() {
         assertFalse(new UnixSocketFactorySupport().isSupported("Windows 10"));
