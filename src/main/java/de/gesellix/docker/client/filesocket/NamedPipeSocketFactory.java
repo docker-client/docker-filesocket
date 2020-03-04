2b1a23df4d73743a79dd3ac714aab304ca5786de
Tobias Gesellchen <tobias@gesellix.de>
2020-03-04 13:38:33 +0100

    Use consistent naming (Npipe -> NamedPipe)

--- src/main/java/de/gesellix/docker/client/filesocket/NamedPipeSocketFactory.java
+++ src/main/java/de/gesellix/docker/client/filesocket/NamedPipeSocketFactory.java
@@ -2,7 +2,7 @@ package de.gesellix.docker.client.filesocket;
 
 import java.net.Socket;
 
-public class NpipeSocketFactory extends FileSocketFactory {
+public class NamedPipeSocketFactory extends FileSocketFactory {
 
     @Override
     public Socket createSocket() {
---
+++ src/test/java/de/gesellix/docker/client/filesocket/NamedPipeSocketFactoryTest.java
@@ -0,0 +1,13 @@
+package de.gesellix.docker.client.filesocket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NamedPipeSocketFactoryTest {
+
+    @Test
+    void createsNamedPipeSocket() {
+        assertTrue(new NamedPipeSocketFactory().createSocket() instanceof NamedPipeSocket);
+    }
+}
