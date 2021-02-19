b0745ed7ddd0503606a47b85d5d20c2e0427a98d
Tobias Gesellchen <tobias@gesellix.de>
2021-02-19 23:28:53 +0100

    Replace Bintray with Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,109 +1,105 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import java.text.SimpleDateFormat
 import java.util.*
 
-rootProject.extra.set("artifactVersion", SimpleDateFormat("yyyy-MM-dd\'T\'HH-mm-ss").format(Date()))
-rootProject.extra.set("bintrayDryRun", false)
-
-buildscript {
-    repositories {
-        mavenLocal()
-        jcenter()
-        mavenCentral()
-    }
-}
-
 plugins {
-    id("java-library")
-    id("maven-publish")
-    id("com.github.ben-manes.versions") version "0.33.0"
-    id("com.jfrog.bintray") version "1.8.5"
-    id("net.ossindex.audit") version "0.4.11"
-    id("io.freefair.github.package-registry-maven-publish") version "5.2.1"
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-val dependencyVersions = listOf<String>()
-
-configurations.all {
-    resolutionStrategy {
-        failOnVersionConflict()
-        force(dependencyVersions)
-    }
+  id("java-library")
+  id("maven-publish")
+  id("signing")
+  id("com.github.ben-manes.versions") version "0.36.0"
+  id("net.ossindex.audit") version "0.4.11"
+  id("io.freefair.maven-central.validate-poms") version "5.3.0"
+  id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 repositories {
-    mavenLocal()
-    jcenter()
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    constraints {
-        implementation("org.slf4j:slf4j-api") {
-            version {
-                strictly("[1.7,1.8)")
-                prefer("1.7.30")
-            }
-        }
-        implementation("com.squareup.okio:okio") {
-            version {
-                strictly("[2.5,3)")
-                prefer("2.8.0")
-            }
-        }
-        api("com.squareup.okhttp3:okhttp") {
-            version {
-                strictly("[4,5)")
-                prefer("4.9.0")
-            }
-        }
-        listOf("com.kohlschutter.junixsocket:junixsocket-core",
-                "com.kohlschutter.junixsocket:junixsocket-common").onEach {
-            implementation(it) {
-                version {
-                    strictly("[2.3,3)")
-                    prefer("2.3.2")
-                }
-            }
-        }
-        listOf("org.jetbrains.kotlin:kotlin-stdlib",
-                "org.jetbrains.kotlin:kotlin-stdlib-common").onEach {
-            implementation(it) {
-                version {
-                    strictly("[1.3,1.5)")
-                    prefer("1.4.10")
-                }
-            }
-        }
+  constraints {
+    implementation("org.slf4j:slf4j-api") {
+      version {
+        strictly("[1.7,1.8)")
+        prefer("1.7.30")
+      }
     }
-    implementation("org.slf4j:slf4j-api")
-    testRuntimeOnly("org.slf4j:jul-to-slf4j:[1.7,1.8)!!1.7.30")
-    testRuntimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.2.3")
+    implementation("com.squareup.okio:okio") {
+      version {
+        strictly("[2.5,3)")
+        prefer("2.8.0")
+      }
+    }
+    api("com.squareup.okhttp3:okhttp") {
+      version {
+        strictly("[4,5)")
+        prefer("4.9.0")
+      }
+    }
+    listOf(
+      "com.kohlschutter.junixsocket:junixsocket-core",
+      "com.kohlschutter.junixsocket:junixsocket-common"
+    ).onEach {
+      implementation(it) {
+        version {
+          strictly("[2.3,3)")
+          prefer("2.3.2")
+        }
+      }
+    }
+    listOf(
+      "org.jetbrains.kotlin:kotlin-stdlib",
+      "org.jetbrains.kotlin:kotlin-stdlib-common"
+    ).onEach {
+      implementation(it) {
+        version {
+          strictly("[1.3,1.5)")
+          prefer("1.4.10")
+        }
+      }
+    }
+  }
+  implementation("org.slf4j:slf4j-api")
+  testRuntimeOnly("org.slf4j:jul-to-slf4j:[1.7,1.8)!!1.7.30")
+  testRuntimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.2.3")
 
-    api("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okio:okio")
+  api("com.squareup.okhttp3:okhttp")
+  implementation("com.squareup.okio:okio")
 
-    implementation("com.kohlschutter.junixsocket:junixsocket-core")
-    implementation("com.kohlschutter.junixsocket:junixsocket-common")
+  implementation("com.kohlschutter.junixsocket:junixsocket-core")
+  implementation("com.kohlschutter.junixsocket:junixsocket-common")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.7.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.7.0")
+}
+
+val dependencyVersions = listOf<String>(
+)
+
+val dependencyGroupVersions = mapOf<String, String>()
+
+configurations.all {
+  resolutionStrategy {
+    failOnVersionConflict()
+    force(dependencyVersions)
+    eachDependency {
+      val forcedVersion = dependencyGroupVersions[requested.group]
+      if (forcedVersion != null) {
+        useVersion(forcedVersion)
+      }
+    }
+  }
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
-    withType(Test::class.java) {
-        useJUnitPlatform()
-    }
-
-    bintrayUpload {
-        dependsOn("build")
-    }
+  withType(Test::class.java) {
+    useJUnitPlatform()
+  }
 
   wrapper {
     gradleVersion = "6.8.2"
@@ -111,49 +107,89 @@ tasks {
   }
 }
 
+val javadocJar by tasks.registering(Jar::class) {
+  dependsOn("classes")
+  archiveClassifier.set("javadoc")
+  from(tasks.javadoc)
+}
+
 val sourcesJar by tasks.registering(Jar::class) {
-    dependsOn("classes")
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
+  dependsOn("classes")
+  archiveClassifier.set("sources")
+  from(sourceSets.main.get().allSource)
 }
 
 artifacts {
-    add("archives", sourcesJar.get())
-}
-
-val publicationName = "dockerFilesocket"
-publishing {
-    publications {
-        register(publicationName, MavenPublication::class) {
-            groupId = "de.gesellix"
-            artifactId = "docker-filesocket"
-            version = rootProject.extra["artifactVersion"] as String
-            from(components["java"])
-            artifact(sourcesJar.get())
-        }
-    }
+  add("archives", sourcesJar.get())
+  add("archives", javadocJar.get())
 }
 
 fun findProperty(s: String) = project.findProperty(s) as String?
 
-rootProject.github {
-    slug.set("${project.property("github.package-registry.owner")}/${project.property("github.package-registry.repository")}")
-    username.set(System.getenv("GITHUB_ACTOR") ?: findProperty("github.package-registry.username"))
-    token.set(System.getenv("GITHUB_TOKEN") ?: findProperty("github.package-registry.password"))
+val isSnapshot = project.version == "unspecified"
+val artifactVersion = if (!isSnapshot) project.version as String else SimpleDateFormat("yyyy-MM-dd\'T\'HH-mm-ss").format(Date())!!
+val publicationName = "dockerFilesocket"
+publishing {
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/${property("github.package-registry.owner")}/${property("github.package-registry.repository")}")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR") ?: findProperty("github.package-registry.username")
+        password = System.getenv("GITHUB_TOKEN") ?: findProperty("github.package-registry.password")
+      }
+    }
+  }
+  publications {
+    register(publicationName, MavenPublication::class) {
+      pom {
+        name.set("docker-filesocket")
+        description.set("Unix Domain Sockets and Named Pipes for the JVM on Linux, macOS, and Windows")
+        url.set("https://github.com/docker-client/docker-filesocket")
+        licenses {
+          license {
+            name.set("MIT")
+            url.set("https://opensource.org/licenses/MIT")
+          }
+        }
+        developers {
+          developer {
+            id.set("gesellix")
+            name.set("Tobias Gesellchen")
+            email.set("tobias@gesellix.de")
+          }
+        }
+        scm {
+          connection.set("scm:git:github.com/docker-client/docker-filesocket.git")
+          developerConnection.set("scm:git:ssh://github.com/docker-client/docker-filesocket.git")
+          url.set("https://github.com/docker-client/docker-filesocket")
+        }
+      }
+      artifactId = "docker-filesocket"
+      version = artifactVersion
+      from(components["java"])
+      artifact(sourcesJar.get())
+      artifact(javadocJar.get())
+    }
+  }
 }
 
-bintray {
-    user = System.getenv()["BINTRAY_USER"] ?: findProperty("bintray.user")
-    key = System.getenv()["BINTRAY_API_KEY"] ?: findProperty("bintray.key")
-    setPublications(publicationName)
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "docker-utils"
-        name = "docker-filesocket"
-        desc = "Unix Domain Sockets and Named Pipes for the JVM on Linux, macOS, and Windows"
-        setLicenses("Apache-2.0")
-        setLabels("docker", "unix socket", "linux", "mac", "named pipe", "windows", "engine api", "remote api", "client", "java")
-        version.name = rootProject.extra["artifactVersion"] as String
-        vcsUrl = "https://github.com/docker-client/docker-filesocket.git"
-    })
-    dryRun = rootProject.extra["bintrayDryRun"] as Boolean
+signing {
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  useInMemoryPgpKeys(signingKey, signingPassword)
+  sign(publishing.publications[publicationName])
+}
+
+nexusPublishing {
+  repositories {
+    if (!isSnapshot) {
+      sonatype {
+        // 'sonatype' is pre-configured for Sonatype Nexus (OSSRH) which is used for The Central Repository
+        stagingProfileId.set(System.getenv("SONATYPE_STAGING_PROFILE_ID") ?: findProperty("sonatype.staging.profile.id")) //can reduce execution time by even 10 seconds
+        username.set(System.getenv("SONATYPE_USERNAME") ?: findProperty("sonatype.username"))
+        password.set(System.getenv("SONATYPE_PASSWORD") ?: findProperty("sonatype.password"))
+      }
+    }
+  }
 }
