d8b072ea219d30edca319a68c888e324a3d7bb86
Tobias Gesellchen <tobias@gesellix.de>
2021-04-07 21:01:46 +0200

    simplify dependency syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ dependencies {
     }
   }
   implementation("org.slf4j:slf4j-api")
-  testRuntimeOnly("org.slf4j:jul-to-slf4j:[1.7,1.8)!!1.7.30")
-  testRuntimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.2.3")
+  testRuntimeOnly("org.slf4j:jul-to-slf4j:1.7.30")
+  testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
 
   api("com.squareup.okhttp3:okhttp")
   implementation("com.squareup.okio:okio")
