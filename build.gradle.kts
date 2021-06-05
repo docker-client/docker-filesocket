e6ba15c22c38166afbf4ad31adf772d99f344aa1
Tobias Gesellchen <tobias@gesellix.de>
2021-06-05 15:40:56 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
       implementation(it) {
         version {
           strictly("[1.3,1.5)")
-          prefer("1.4.10")
+          prefer("1.4.30")
         }
       }
     }
@@ -68,9 +68,9 @@ dependencies {
   implementation("com.kohlschutter.junixsocket:junixsocket-core")
   implementation("com.kohlschutter.junixsocket:junixsocket-common")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.7.1")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.7.2")
 }
 
 val dependencyVersions = listOf<String>(
