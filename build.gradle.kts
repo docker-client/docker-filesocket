099626e73381602897032b3fc87975d325b558f3
Tobias Gesellchen <tobias@gesellix.de>
2021-07-26 21:05:59 +0200

    prefer slf4j 1.7.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api") {
       version {
         strictly("[1.7,1.8)")
-        prefer("1.7.30")
+        prefer("1.7.32")
       }
     }
     implementation("com.squareup.okio:okio") {
