7cd61ba81d43da9a05f60437f2b442fb0bddddbb
Tobias Gesellchen <tobias@gesellix.de>
2022-02-13 20:16:20 +0100

    write dependency versions in published Maven POM

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,15 +58,15 @@ dependencies {
       }
     }
   }
-  implementation("org.slf4j:slf4j-api")
+  implementation("org.slf4j:slf4j-api:1.7.36")
   testRuntimeOnly("org.slf4j:jul-to-slf4j:1.7.36")
   testRuntimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.2.10")
 
-  api("com.squareup.okhttp3:okhttp")
-  implementation("com.squareup.okio:okio")
+  api("com.squareup.okhttp3:okhttp:4.9.3")
+  implementation("com.squareup.okio:okio:3.0.0")
 
-  implementation("com.kohlschutter.junixsocket:junixsocket-core")
-  implementation("com.kohlschutter.junixsocket:junixsocket-common")
+  implementation("com.kohlschutter.junixsocket:junixsocket-core:2.4.0")
+  implementation("com.kohlschutter.junixsocket:junixsocket-common:2.4.0")
 
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
