f830669f1e7e1ffe2f90c0a3d2f1b312d668cc0d
Tobias Gesellchen <tobias@gesellix.de>
2023-07-02 11:37:12 +0200

    Bump preferred logback-classic dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
   }
   implementation("org.slf4j:slf4j-api:2.0.7")
   testRuntimeOnly("org.slf4j:jul-to-slf4j:2.0.7")
-  testRuntimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.3.6")
+  testRuntimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.3.8")
 
   api("com.squareup.okhttp3:okhttp:4.11.0")
   implementation("com.squareup.okio:okio-jvm:3.3.0")
