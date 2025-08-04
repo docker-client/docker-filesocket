88750fbd38e6200583493a15c9bd5065df4674d3
Tobias Gesellchen <tobias@gesellix.de>
2025-08-04 20:54:46 +0200

    wip

--- src/test/java/de/gesellix/docker/client/filesocket/UpgradeClient.java
+++ src/test/java/de/gesellix/docker/client/filesocket/UpgradeClient.java
@@ -3,6 +3,9 @@ package de.gesellix.docker.client.filesocket;
 import static de.gesellix.docker.client.filesocket.FileSocket.SOCKET_MARKER;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 import okhttp3.Call;
@@ -19,6 +22,26 @@ import okio.Source;
 public class UpgradeClient {
 
   public static void main(String[] args) throws Exception {
+    String pipePath = "\\\\.\\pipe\\java_echo_test";
+    NamedPipeSocket client = new NamedPipeSocket();
+    client.connect(pipePath);
+
+    OutputStream os = client.getOutputStream();
+    os.write("line1\n".getBytes(StandardCharsets.UTF_8));
+    os.flush();
+    Thread.sleep(1000);
+    os.write("line2\n".getBytes(StandardCharsets.UTF_8));
+    os.flush();
+
+    InputStream is = client.getInputStream();
+    byte[] buf = new byte[1024];
+    int read;
+    while ((read = is.read(buf)) != -1) {
+      System.out.println("Java got: " + new String(buf, 0, read));
+    }
+  }
+
+  public static void main2(String[] args) throws Exception {
     //    String host = "localhost";
     //    int port = 8080;
     //    HttpUrl url = new HttpUrl.Builder()
@@ -27,10 +50,10 @@ public class UpgradeClient {
     //        .port(port)
     //        .build();
 
-    //    NamedPipeSocketFactory socketFactory = new NamedPipeSocketFactory();
-    //    String socketAddress = "//./pipe/docker_engine";
-    UnixSocketFactory socketFactory = new UnixSocketFactory();
-    String socketAddress = "/var/run/docker.sock";
+        NamedPipeSocketFactory socketFactory = new NamedPipeSocketFactory();
+        String socketAddress = "//./pipe/docker_engine";
+//    UnixSocketFactory socketFactory = new UnixSocketFactory();
+//    String socketAddress = "/var/run/docker.sock";
 
     // docker run --rm --name hijacking -it alpine:edge cat
     // docker run --rm --name hijacking -it --entrypoint /cat gesellix/echo-server:2025-07-27T22-12-00
